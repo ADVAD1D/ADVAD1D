ec8190f75d525e30990b6814eaf3f3431317621f
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2025-09-05 10:15:06 -0500

    new asteroid spawner script

--- Scripts/asteroid_spawner.gd
+++ Scripts/asteroid_spawner.gd
@@ -1,36 +1,30 @@
 extends Marker2D
 
 @export var asteroid_scene: PackedScene 
-@export var spawn_margin: float = 1680.0
+@export var spawn_margin: float = 1200.0
 @export var target_radius: float = 250.0
 @export var player_node: CharacterBody2D
-@export var safe_radius: float = 200.0
-
-var screen_size: Vector2
-
-func _ready() -> void:
-	screen_size = get_viewport().get_visible_rect().size
+@export var safe_radius: float = 1250.0
 
 func create_asteroid():
-	
 	if not is_instance_valid(player_node):
 		return
-	var spawn_position: Vector2
-	var is_position_safe: bool = false
-	var center = screen_size / 2
 		
-	while not is_position_safe:
-		var spawn_radius = center.length() + spawn_margin
-		var random_angle = randf_range(0, TAU)
-		spawn_position = center + Vector2.RIGHT.rotated(random_angle) * spawn_radius
-			
-		if spawn_position.distance_to(player_node.global_position) > safe_radius:
-			is_position_safe = true
-				
-				
+	var viewport_rect = get_viewport().get_visible_rect()
+	var center = viewport_rect.size / 2
+	var spawn_radius = viewport_rect.size.length() / 2 + spawn_margin 
+	var random_angle = randf_range(0, TAU)
+	var spawn_position = center + Vector2.RIGHT.rotated(random_angle) * spawn_radius
+	
+	var distance_to_player = spawn_position.distance_to(player_node.global_position)
+	
+	if distance_to_player < safe_radius:
+		var direction_from_player = (spawn_position - player_node.global_position).normalized()
+		spawn_position = player_node.global_position + direction_from_player * safe_radius 
+	
 	var asteroid_instance: Area2D = asteroid_scene.instantiate()
 	get_parent().add_child(asteroid_instance)
-	asteroid_instance.position = spawn_position
+	asteroid_instance.global_position = spawn_position
 	
 	var random_offset = Vector2(
 		randf_range(-target_radius, target_radius),
