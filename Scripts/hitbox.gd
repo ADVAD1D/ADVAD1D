a2400046ac937478f6361db3fe9e1c71a0afe4d5
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2025-09-10 17:50:54 -0500

    fix scores in enemies death

--- Scripts/hitbox.gd
+++ Scripts/hitbox.gd
@@ -1,7 +1,6 @@
 extends Area2D
-signal hit
+signal hit(area_collided: Area2D)
 
 func _on_area_entered(area: Area2D) -> void:
-	if area.is_in_group("lasers"):
-		hit.emit()
-		area.queue_free()
+	if (area.is_in_group("lasers") or area.is_in_group("enemies_death")):
+		hit.emit(area)
