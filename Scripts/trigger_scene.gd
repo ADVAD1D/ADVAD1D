4dc6a9b92651f7b2a7d9d59b91e524d3431a707c
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2025-10-10 19:15:34 -0500

    trigger scene and ending

---
+++ Scripts/trigger_scene.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+# En el Inspector, podrás escribir la ruta a la escena que quieres cargar.
+@export var target_scene_path: PackedScene
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node2D):
+	print("Body entered trigger: ", body.name)
+	if body.is_in_group("player"):
+		print("¡Jugador detectado! Cambiando de escena a: ", target_scene_path)
+		
+		if not target_scene_path.is_empty():
+			get_tree().change_scene_to_packed(target_scene_path)
