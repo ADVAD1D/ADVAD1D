61acb6f2009637ae46bd6b42b39bbb7d77f7e983
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2025-10-16 18:35:14 -0500

    Crediits panel

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -5,8 +5,10 @@ extends Control
 
 @onready var animated_background: AnimatedSprite2D = $Background
 @onready var play_button: TextureButton = $VBoxContainer/PlayButton
+@onready var credits_button: TextureButton = $VBoxContainer/CreditsButton
 @onready var quit_button: TextureButton = $VBoxContainer/QuitButton
 @onready var button_sound: AudioStreamPlayer = $ButtonSound
+@onready var credits_panel: Control = $CreditsPanel
 
 var is_scrolling: bool = true
 
@@ -14,6 +16,7 @@ func _ready() -> void:
 	GameManager.can_pause = false
 	play_button.pressed.connect(_on_play_button_pressed)
 	quit_button.pressed.connect(_on_quit_button_pressed)
+	credits_button.pressed.connect(_on_credits_button_pressed)
 
 func _process(delta: float) -> void:
 	if is_scrolling:
@@ -27,9 +30,15 @@ func _on_quit_button_pressed():
 
 func _on_play_button_mouse_entered() -> void:
 	button_sound.play()
+	
+func _on_credits_button_pressed():
+	credits_panel.show()
 
 func _on_quit_button_mouse_entered() -> void:
 	button_sound.play() # Replace with function body.
 
 func _on_scroll_timer_timeout() -> void:
 	is_scrolling = false # Replace with function body.
+
+func _on_credits_button_mouse_entered() -> void:
+	button_sound.play() # Replace with function body.
