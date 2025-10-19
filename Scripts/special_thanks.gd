94e59dbf1d13a965b34f2e941cfe81d2b7fe6cf6
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2025-10-19 15:12:28 -0500

    back sound

--- Scripts/special_thanks.gd
+++ Scripts/special_thanks.gd
@@ -2,10 +2,15 @@ extends Control
 
 @onready var close_button: TextureButton = %CloseButton
 @onready var button_sound: AudioStreamPlayer = $ButtonSound
+@onready var back_sound: AudioStreamPlayer = $BackSound
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	close_button.pressed.connect(hide) # Replace with function body.
+	close_button.pressed.connect(_on_close_button_pressed)
 
 func _on_close_button_mouse_entered() -> void:
 	button_sound.play() # Replace with function body.
+	
+func _on_close_button_pressed():
+	back_sound.play()
