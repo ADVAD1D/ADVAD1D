a4917de93703f5f707fbddb47fe7be7dc51e70ca
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2025-11-11 18:22:52 -0500

    collision with body entered in player

--- Scripts/phase_manager.gd
+++ Scripts/phase_manager.gd
@@ -9,8 +9,8 @@ signal timer_updated(time_left_string)
 @export var ship_enemy_spawner: Node2D
 @export var saw_enemy_spawner: Node2D
 
-@export var min_shoot_timerate: float = 0.2 # = 0.2
-@export var max_shoot_timerate: float = 0.5 # = 0.5
+@export var min_shoot_timerate: float = 5.2 # = 0.2
+@export var max_shoot_timerate: float = 5.5 # = 0.5
 
 @export var min_ship_enemies: float = 2.0 # = 2.0
 @export var max_ship_enemies: float = 5.0 # = 5.0
