d70847dd0be63f25bcb604aa6256a10fabad5278
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2025-11-17 10:50:43 -0500

    manage browser support for discord_manager

--- addons/discord-rpc-gd/nodes/discord_autoload.gd
+++ addons/discord-rpc-gd/nodes/discord_autoload.gd
@@ -5,21 +5,29 @@
 ##
 ## @tutorial: https://github.com/vaporvee/discord-rpc-godot/wiki
 extends Node
+var browser_support: bool = GameManager.browser_support
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if browser_support == false:
 	
-	#the parser takes the env value as a string 
-	var discord_app_id = EnvParser.parse("DISCORD_APP_ID")
-	
-	if discord_app_id:
-		DiscordRPC.app_id = int(discord_app_id) # Replace with function body.
-		DiscordRPC.large_image = "portada" # Image key from "Art Assets"
-		DiscordRPC.start_timestamp = int(Time.get_unix_time_from_system()) # "02:46 elapsed"
-		# DiscordRPC.end_timestamp = int(Time.get_unix_time_from_system()) + 3600 # +1 hour in unix time / "01:00:00 remaining"
-		DiscordRPC.refresh() # Always refresh after changing the values!
+		#the parser takes the env value as a string 
+		var discord_app_id = EnvParser.parse("DISCORD_APP_ID")
+		
+		if discord_app_id:
+			DiscordRPC.app_id = int(discord_app_id) # Replace with function body.
+			DiscordRPC.large_image = "portada" # Image key from "Art Assets"
+			DiscordRPC.start_timestamp = int(Time.get_unix_time_from_system()) # "02:46 elapsed"
+			# DiscordRPC.end_timestamp = int(Time.get_unix_time_from_system()) + 3600 # +1 hour in unix time / "01:00:00 remaining"
+			DiscordRPC.refresh() # Always refresh after changing the values!
+		else:
+			print("Error, no se encontró el valor DISCORD_APP_ID en el archivo .env")
+			
 	else:
-		print("Error, no se encontró el valor DISCORD_APP_ID en el archivo .env")
+		return
 
 func  _process(_delta) -> void:
-	DiscordRPC.run_callbacks()
+	if browser_support == false:
+		DiscordRPC.run_callbacks()
+	else:
+		return
