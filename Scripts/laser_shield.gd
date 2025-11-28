d6d1a24fd470422b057e6cf7cfe69c840fcf9021
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2025-11-28 11:09:27 -0500

    laser shield break animation

--- Scripts/laser_shield.gd
+++ Scripts/laser_shield.gd
@@ -4,14 +4,20 @@ extends Area2D
 @export var duration_timer: Timer
 
 @onready var spawn_time: float = 0.1
+@onready var metal_sound: AudioStreamPlayer2D = $BreakSound
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	play_all("default") # Replace with function body.
-	duration_timer.timeout.connect(queue_free)
+func _ready() -> void: # Replace with function body.
+	duration_timer.timeout.connect(_on_timeout)
 	duration_timer.start()
 	area_entered.connect(_on_area_entered)
 	var tween = create_tween()
 	tween.tween_property(self, "scale", Vector2(1.5, 1.5), spawn_time).from(Vector2.ZERO)
+	
+func _on_timeout():
+	set_deferred("monitoring", false)
+	set_deferred("monitorable", false)
+	metal_sound.play()
+	queue_free()
 
 func play_all(animation_name: String):
 	for sprite in animated_sprites:
@@ -20,6 +26,8 @@ func play_all(animation_name: String):
 		
 func _on_area_entered(area: Area2D):
 	if area.is_in_group("enemy_laser"):
+		play_all("break")
+		metal_sound.play()
 		if area.has_method("set_direction"):
 			area.set_direction(-area.direction)
 			#change the group
