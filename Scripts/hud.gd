eb3d0be4e21c56abe22191a079881fa607e0b0e2
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2026-03-30 19:49:08 -0500

    add fps toggled logic in hud

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -1,16 +1,19 @@
 extends Control
 
+@onready var show_fps: bool = GameManager.show_fps
+
 @onready var objective_label: Label = $ObjectiveLabel
+@onready var fps_label: Label = $FPSContainer/FPSLabel
 @onready var speedrun_label: Label = $SpeedrunLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var phase_manager = get_tree().root.get_node("Main/PhaseNode") # Replace with function body.
+	fps_toggled()
 	if not GameManager.speedrun_mode_active:
 		speedrun_label.hide()
 	else:
 		speedrun_label.show()
-		
 	if phase_manager:
 		phase_manager.phase_started.connect(_on_phase_started)
 	else:
@@ -22,3 +25,9 @@ func _on_phase_started(_phase_number: int, score_requirement: int):
 func _process(_delta: float) -> void:
 	if GameManager.speedrun_mode_active:
 		speedrun_label.text = GameManager.get_formatted_speedrun_time()
+		
+func fps_toggled():
+	if show_fps == false:
+		fps_label.hide()
+	else:
+		return
