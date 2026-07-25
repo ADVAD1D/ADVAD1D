4dfac83656ba2b20079119250a7f48a2d4c3d99c
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2026-07-25 16:25:45 -0500

    Allow instant shield pickup if standing on it when shield breaks

--- Scripts/laser_shield_pickup.gd
+++ Scripts/laser_shield_pickup.gd
@@ -8,26 +8,34 @@ var base_y_position: float
 var spawn_time: float = 0.2
 @onready var sprite: Sprite2D = $Sprite2D
 
+var is_collected: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	body_entered.connect(_on_body_entered) # Replace with function body.
-	
 	scale = Vector2.ZERO
 	var tween = create_tween()
 	tween.set_pause_mode(Tween.TWEEN_PAUSE_BOUND)
 	tween.tween_property(self, "scale", Vector2(4.5, 4.5), spawn_time)
 	
+func _physics_process(_delta: float) -> void:
+	if is_collected:
+		return
+	for body in get_overlapping_bodies():
+		if body.is_in_group("player"):
+			var has_shield = false
+			for child in body.get_children():
+				if child.is_in_group("player_shield"):
+					has_shield = true
+					break
+			if not has_shield:
+				is_collected = true
+				call_deferred("_equip_shield", body)
+				break
+				
 func _process(_delta: float) -> void:
 	var time = Time.get_ticks_msec() * 0.001
 	var displacement = sin(time * bob_speed) * bob_amplitude
 	sprite.position.y = base_y_position + displacement
-
-func _on_body_entered(body: Node2D):
-	if body.is_in_group("player"):
-		for child in body.get_children():
-			if child.is_in_group("player_shield"):
-				return
-		call_deferred("_equip_shield", body)
 func _equip_shield(player_node):
 	var shield = reward_scene.instantiate()
 	player_node.add_child(shield)
