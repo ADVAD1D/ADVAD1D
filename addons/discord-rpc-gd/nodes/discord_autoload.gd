12877022b00afa67bbd43722a776640b6948abf6
ANGELUSD11 <angelleonardohern3@gmail.com>
2026-09-09 13:30:18 -0500

    Fix: Optimize Discord RPC loading and fix README image paths
        - Hardcoded Discord App ID in discord_autoload.gd (Client IDs are public and safe to expose), removing the need for .env parsing.
        - Added platform checks to completely disable Discord RPC initialization and processing on Web and Mobile (Android/iOS) to save resources.

--- addons/discord-rpc-gd/nodes/discord_autoload.gd
+++ addons/discord-rpc-gd/nodes/discord_autoload.gd
@@ -5,29 +5,30 @@
 ##
 ## @tutorial: https://github.com/vaporvee/discord-rpc-godot/wiki
 extends Node
-var browser_support: bool = GameManager.browser_support
+
+var is_desktop: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if browser_support == false:
+	# Ignore Discord RPC process entirely on Web and Mobile
+	if OS.has_feature("web") or OS.has_feature("mobile") or GameManager.mobile_mode_active or GameManager.force_web_mode:
+		is_desktop = false
+		set_process(false)
+		return
 	
-		#the parser takes the env value as a string 
-		var discord_app_id = EnvParser.parse("DISCORD_APP_ID")
-		
-		if discord_app_id:
-			DiscordRPC.app_id = int(discord_app_id) # Replace with function body.
-			DiscordRPC.large_image = "portada" # Image key from "Art Assets"
-			DiscordRPC.start_timestamp = int(Time.get_unix_time_from_system()) # "02:46 elapsed"
-			# DiscordRPC.end_timestamp = int(Time.get_unix_time_from_system()) + 3600 # +1 hour in unix time / "01:00:00 remaining"
-			DiscordRPC.refresh() # Always refresh after changing the values!
-		else:
-			print("Error, no se encontró el valor DISCORD_APP_ID en el archivo .env")
-			
+	# NOTE: The Discord App ID (Client ID) is public information required by the Discord client.
+	# It is completely safe to hardcode it here. It is NOT a secret token or key.
+	var discord_app_id = 1429848946213126245
+	
+	if discord_app_id:
+		DiscordRPC.app_id = int(discord_app_id) # Replace with function body.
+		DiscordRPC.large_image = "portada" # Image key from "Art Assets"
+		DiscordRPC.start_timestamp = int(Time.get_unix_time_from_system()) # "02:46 elapsed"
+		# DiscordRPC.end_timestamp = int(Time.get_unix_time_from_system()) + 3600 # +1 hour in unix time / "01:00:00 remaining"
+		DiscordRPC.refresh() # Always refresh after changing the values!
 	else:
-		return
+		print("Error, no se encontró el valor DISCORD_APP_ID en el archivo .env")
 
-func  _process(_delta) -> void:
-	if browser_support == false:
+func _process(_delta) -> void:
+	if is_desktop:
 		DiscordRPC.run_callbacks()
-	else:
-		return
